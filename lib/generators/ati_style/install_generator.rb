14c73fa8ffb28357eabd1b55a5e04f86da73905d
Jakson Moura <jaksonrochelly@Jaksons-MacBook-Pro.local>
2019-07-02 09:19:34 -0300

    Renomea templates adicionando .tt ao final depois de copiar

--- lib/generators/ati_style/install_generator.rb
+++ lib/generators/ati_style/install_generator.rb
@@ -3,11 +3,16 @@ module AtiStyle
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../../..",__FILE__)
 
-
       def copy_views
         directory 'app/', 'app/'
         directory 'public/', 'public/'
         directory 'templates/', 'lib/templates/'
+
+        # Rename template files addin .tt
+        path = 'lib/templates/erb/scaffold'
+        Dir.open(path).each do |f|
+          File.rename(f, "#{f}.tt")
+        end
       end
 
     end
