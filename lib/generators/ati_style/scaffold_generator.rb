5d18d86499b47b8795eb62cfd6617d13e49e12db
Jakson Moura <jaksonrochelly@Jaksons-MacBook-Pro.local>
2019-07-02 09:26:59 -0300

    Adiciona método apenas para copiar scaffold

---
+++ lib/generators/ati_style/scaffold_generator.rb
@@ -0,0 +1,18 @@
+module AtiStyle
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../../..",__FILE__)
+
+      def copy_views
+        directory 'templates/', 'lib/templates/'
+
+        # Rename template files addin .tt
+        path = 'lib/templates/erb/scaffold'
+        Dir.open(path).each do |f|
+          File.rename(f, "#{f}.tt")
+        end
+      end
+
+    end
+  end
+end
