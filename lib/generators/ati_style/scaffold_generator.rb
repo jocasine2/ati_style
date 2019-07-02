fed624858556710b29c0b3181afb1b05cd6bf7b1
Jakson Moura <jaksonrochelly@Jaksons-MacBook-Pro.local>
2019-07-02 09:31:35 -0300

    Adicionado descrição do que está acontecendo

--- lib/generators/ati_style/scaffold_generator.rb
+++ lib/generators/ati_style/scaffold_generator.rb
@@ -4,9 +4,14 @@ module AtiStyle
       source_root File.expand_path("../../../..",__FILE__)
 
       def copy_views
+        puts "=================================="
+        puts " INICIANDO INSTALAÇÃO DO TEMPLATE"
+        puts "==================================\n"
+        puts "Copiando templates scaffold...\n"
         directory 'templates/', 'lib/templates/'
 
         # Rename template files addin .tt
+        puts "Difinindo arquivos templates scaffold...\n"
         path = 'lib/templates/erb/scaffold'
         Dir.open(path).each do |f|
           File.rename(f, "#{f}.tt")
