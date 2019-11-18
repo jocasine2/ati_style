498414ed9758ad49a5269bb60ef0b78d4cdf2960
Jakson Moura <jaksonrochelly@Jaksons-MacBook-Pro.local>
2019-11-18 09:14:21 -0300

    Novo toastr

---
+++ app/assets/javascripts/toastr_custom.js
@@ -0,0 +1,22 @@
+toastr.options = {
+    "closeButton": true,
+  "debug": true,
+  "newestOnTop": false,
+  "progressBar": true,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": true,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "5000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "slideDown",
+  "hideMethod": "slideUp"
+  }
+
+// toastr.options.onShown = function() { console.log('hello'); }
+// toastr.options.onHidden = function() { console.log('goodbye'); }
+// toastr.options.onclick = function() { console.log('clicked'); }
+// toastr.options.onCloseClick = function() { console.log('close button clicked'); }
--- app/helpers/materialize_ati_helper.rb
+++ app/helpers/materialize_ati_helper.rb
@@ -2,4 +2,9 @@ module MaterializeAtiHelper
 	def title(page_title)
     content_for(:title) { page_title }
   end
+
+# Mover esse trecho de código para o ApplicationController
+  def toastr(type, body)
+		flash["#{type}"] = body
+	end
 end
